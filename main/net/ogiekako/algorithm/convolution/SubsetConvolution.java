9bbca780bb6bef712bb6e4dc12fd1e5911ed6a9d
Keigo Oka <ogiekako@gmail.com>
2013-11-29 23:02:18 +0900

    Refactor SubsetConvolution

--- main/net/ogiekako/algorithm/convolution/SubsetConvolution.java
+++ main/net/ogiekako/algorithm/convolution/SubsetConvolution.java
@@ -5,27 +5,12 @@ package net.ogiekako.algorithm.convolution;
 [ T \subset S ] = [ \bar{S} \subset \bar{T} ]
  */
 public class SubsetConvolution {
-
     /*
     f.length = 1<<N.
     res[T] = sum_{S \subset T} f[S].
     */
     public static long[] zetaConvolution(long[] f, int N) {
-        long[] g = new long[1 << N];
-        _zetaConvolution(f, g, N - 1, 0);
-        return g;
-    }
-
-    private static void _zetaConvolution(long[] f, long[] g, int n, int mask) {
-        if (n < 0) {
-            g[mask] = f[mask];
-            return;
-        }
-        _zetaConvolution(f, g, n - 1, mask);
-        _zetaConvolution(f, g, n - 1, mask | 1 << n);
-        for (int i = 0; i < 1 << n; i++) {
-            g[mask | 1 << n | i] += g[mask | i];
-        }
+        return convolution(f, N, 1, 0);
     }
 
     /*
@@ -33,21 +18,26 @@ public class SubsetConvolution {
     res[T] = sum_{S \subset T} (-1)^|T-S| g[S]
      */
     public static long[] mobiusConvolution(long[] g, int N) {
-        long[] f = new long[1 << N];
-        _mobiusConvolution(f, g, N - 1, 0);
-        return f;
+        return convolution(g, N, -1, 0);
     }
 
-    private static void _mobiusConvolution(long[] f, long[] g, int n, int mask) {
-        if (n < 0) {
-            f[mask] = g[mask];
-            return;
+    /*
+    d = 1 or -1.
+    subset = 0 or 1
+    subset = 1 -> f(T) = sum_{S⊆T}d^|T＼S|g(S)
+    subset = 0 -> f(T) = sum_{T⊆S}d^|T＼S|g(S)
+     */
+    private static long[] convolution(long[] g, int N, int d, int subset) {
+        long[] f = new long[1 << N];
+        System.arraycopy(g, 0, f, 0, f.length);
+        for (int i = 0; i < N; i++) {
+            for (int S = 0; S < 1 << N; S++) {
+                if ((S >> i & 1) == subset) {
+                    f[S ^ (1 << i)] += d * f[S];
+                }
+            }
         }
-        _mobiusConvolution(f, g, n - 1, mask);
-        _mobiusConvolution(f, g, n - 1, mask | 1 << n);
-        for (int i = 0; i < 1 << n; i++) {
-            f[mask | 1 << n | i] -= f[mask | i];
-        }
+        return f;
     }
 
     /*
@@ -55,10 +45,7 @@ public class SubsetConvolution {
     res[T] = sum_{T \subset S} f[S]
      */
     public static long[] zetaConvolutionInv(long[] f, int N) {
-        long[] f2 = functionToComplement(f, N);
-        long[] g2 = zetaConvolution(f2, N);
-        long[] g = functionToComplement(g2, N);
-        return g;
+        return convolution(f, N, 1, 1);
     }
 
     /*
@@ -66,23 +53,6 @@ public class SubsetConvolution {
    res[T] = sum_{T \subset S} (-1)^|S-T|g[S]
     */
     public static long[] mobiusConvolutionInv(long[] g, int N) {
-        long[] g2 = functionToComplement(g, N);
-        long[] f2 = mobiusConvolution(g2, N);
-        long[] f = functionToComplement(f2, N);
-        return f;
-    }
-
-    /*
-    f.length = 1<<N.
-    res[T] = f[ \bar{T} ].
-     */
-    public static long[] functionToComplement(long[] f, int N) {
-        long[] res = new long[1 << N];
-        for (int i = 0; i < 1 << N; i++) res[i] = f[complement(i, N)];
-        return res;
-    }
-
-    private static int complement(int mask, int N) {
-        return ((1 << N) - 1) ^ mask;
+        return convolution(g, N, -1, 1);
     }
 }
