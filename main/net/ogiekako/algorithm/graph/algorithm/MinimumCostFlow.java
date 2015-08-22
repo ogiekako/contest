e78126f6f0e676a9f00684dc1685d5d99e6ffdd3
Keigo Oka <ogiekako@gmail.com>
2015-08-22 20:43:19 +0900

    Add primalDual to MinimumCostFlow.java.

--- main/net/ogiekako/algorithm/graph/algorithm/MinimumCostFlow.java
+++ main/net/ogiekako/algorithm/graph/algorithm/MinimumCostFlow.java
@@ -7,6 +7,7 @@ import net.ogiekako.algorithm.utils.Pair;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.TreeSet;
 
 /**
  * Thm.                                                                              <br></br>
@@ -31,34 +32,44 @@ public class MinimumCostFlow {
     final Graph graph;
     final int n;
     final double[] supply;
-    long[] potential;
-    long cost;
+    double[] potential;
+    double cost;
     Edge[] path;
 
-    MinimumCostFlow(Graph _graph) {
+    public MinimumCostFlow(Graph _graph) {
         this.graph = _graph;
         n = graph.size();
         supply = new double[n];
     }
 
     // returns Long.MAX_VALUE if impossible
+    // Deprecated. Use new MinimumCostFlow(graph).minimumCostFlow(...).
     public static long minimumCostFlow(Graph graph, int source, int sink, long flow) {
-        return new MinimumCostFlow(graph).minimumCostFlow(source, sink, flow);
+        // TOOD: Don't cast to long.
+        return (long) new MinimumCostFlow(graph).minimumCostFlow(source, sink, flow);
     }
 
+    // Deprecated. Use new MinimumCostFlow(graph).minimumCostFlow(...).
     public static long minimumCostCirculation(Graph graph) {
-        return new MinimumCostFlow(graph).minimumCostCirculation();
+        return (long) new MinimumCostFlow(graph).minimumCostCirculation();
     }
 
-    long minimumCostFlow(int source, int sink, long flow) {
+    public double minimumCostFlow(int source, int sink) {
+        return minimumCostFlow(source, sink, Long.MAX_VALUE);
+    }
+
+    // returns Long.MAX_VALUE if impossible
+    // TODO: Return Double.POSITIVE_INFINITY instead of Long.MAX_VALUE.
+    public double minimumCostFlow(int source, int sink, long flow) {
         supply[source] += flow;
         supply[sink] -= flow;
         return minimumCostCirculation();
     }
 
     // returns Long.MAX_VALUE if impossible
-    long minimumCostCirculation() {
-        potential = new long[n];
+    // TODO: Return Double.POSITIVE_INFINITY instead of Long.MAX_VALUE.
+    double minimumCostCirculation() {
+        potential = new double[n];
         cost = 0;
         for (int v = 0; v < n; v++) for (Edge e : graph.edges(v)) if (e.residue() > 0 && e.cost() < 0) cancel(e);
         for (; ; ) {
@@ -81,7 +92,7 @@ public class MinimumCostFlow {
                     if (e.residue() > 0) {
                         int u = e.to();
                         double nDist = (dist + (potential[v] - potential[u])) + e.cost();
-                        if (nDist < distance[u]) {
+                        if (nDist < distance[u] - 1e-9) {
                             distance[u] = nDist;
                             path[u] = e;
                             que.offer(Pair.of(nDist, u));
@@ -112,5 +123,98 @@ public class MinimumCostFlow {
         supply[e.from()] -= e.residue();
         cost += e.residue() * e.cost();
         e.pushFlow(e.residue());
+    }
+
+    /**
+     * Graph shouldn't contain a negative cycle.
+     * If the graph initially has negative cycle(s), those cycle should cancelled before this method is called.
+     * Returns Double.POSITIVE_INFINITY if it's not possible to send `flow' amount for flows.
+     * Otherwise returns the minimum cost.
+     * <p/>
+     * This method computes the result as follows:
+     * - Run Bellman Ford algorithm once to compute initial potentials, which takes O(nm) time.
+     * - Run Dijkstra's algorithm O(flow) times, which takes O(flow (m + n log m)) time.
+     * <p>
+     *     Verified: AOJ 1095 KND Factory (1030ms)
+     * </p>
+     */
+    public double primalDual(final int s, final int t, double flow) {
+        double[] potential = calcInitialPotential(s);
+        double res = 0;
+        Edge[] prev = new Edge[n];
+        Queue<Entry> que = new PriorityQueue<Entry>();
+        while (flow > 0) {
+            double[] costs = new double[n];
+            boolean[] visited = new boolean[n];
+            Arrays.fill(costs, Double.POSITIVE_INFINITY);
+            costs[s] = 0;
+            prev[s] = prev[t] = null;
+            que.clear();
+            que.offer(new Entry(0, s));
+            while (!que.isEmpty()) {
+                Entry cur = que.poll();
+                if (visited[cur.v]) continue;
+                visited[cur.v] = true;
+                for (Edge e : graph.edges(cur.v))
+                    if (e.residue() > 0 && !visited[e.to()]) {
+                        if (costs[e.to()] > costs[cur.v] + e.cost() + potential[cur.v] - potential[e.to()]) {
+                            costs[e.to()] = costs[cur.v] + e.cost() + potential[cur.v] - potential[e.to()];
+                            prev[e.to()] = e;
+                            que.offer(new Entry(costs[e.to()], e.to()));
+                        }
+                    }
+            }
+            if (prev[t] == null) return Double.POSITIVE_INFINITY;
+            for (int i = 0; i < n; i++) {
+                potential[i] = costs[i];
+            }
+            double min = flow;
+            for (Edge e = prev[t]; e != null; e = prev[e.from()]) {
+                min = Math.min(min, e.residue());
+            }
+            for (Edge e = prev[t]; e != null; e = prev[e.from()]) {
+                res += e.cost() * min;
+                e.pushFlow(min);
+            }
+            flow -= min;
+        }
+        return res;
+    }
+
+    class Entry implements Comparable<Entry> {
+        double dist;
+        int v;
+
+        public Entry(double dist, int v) {
+            this.dist = dist;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Entry o) {
+            return Double.compare(dist, o.dist);
+        }
+    }
+
+    private double[] calcInitialPotential(int from) {
+        double[] potential = new double[n];
+        Arrays.fill(potential, Double.POSITIVE_INFINITY);
+        potential[from] = 0;
+        for (int iter = 0; iter < n; iter++) {
+            boolean updated = false;
+            for (int i = 0; i < n; i++) {
+                for (Edge e : graph.edges(i)) {
+                    if (e.residue() <= 0) continue;
+                    if (potential[e.to()] > e.cost() + potential[i]) {
+                        updated = true;
+                        potential[e.to()] = e.cost() + potential[i];
+                    }
+                }
+            }
+            if (!updated) {
+                return potential;
+            }
+        }
+        throw new IllegalArgumentException("Negative Cycle");
     }
 }
