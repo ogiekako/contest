d155b70b666408e34293762e92f1e8c8f9c1a8ec
oka <ogiekako@gmail.com>
2016-12-30 00:26:49 +0900

    Add solutions for SRM704.

---
+++ test/on2016_12/on2016_12_29_Single_Round_Match_704/TreeDistanceConstruction/Main.java
@@ -0,0 +1,14 @@
+package on2016_12.on2016_12_29_Single_Round_Match_704.TreeDistanceConstruction;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("test/on2016_12/on2016_12_29_Single_Round_Match_704/TreeDistanceConstruction/TreeDistanceConstruction.tctask"))
+			Assert.fail();
+	}
+}
