5d2c0f137142141dc4f45f87dc983713f61ca078
Keigo Oka <ogiekako@gmail.com>
2017-05-23 02:29:48 +0900

    Add min, max, quantile, rangefreq, prevvalue, nextvalue to WaveletMatrix.

--- main/net/ogiekako/algorithm/dataStructure/WaveletMatrix.java
+++ main/net/ogiekako/algorithm/dataStructure/WaveletMatrix.java
@@ -11,17 +11,20 @@ public class WaveletMatrix {
     int n;
     FID[] B;
     int[] k;
+    // Maximum value.
+    int M;
 
     /**
+     * s will be modified.
      * Precondition: s[i] >= 0.
      */
     public WaveletMatrix(int[] s) {
         for (int ss : s) if (ss < 0) throw new IllegalArgumentException("ss should be positive but was " + ss);
 
-        int max = 0;
-        for (int ss : s) max = Math.max(max, ss);
+        M = 0;
+        for (int ss : s) M = Math.max(M, ss);
         D = 1;
-        while (1L << D <= max) D++;
+        while (1L << D <= M) D++;
 
         n = s.length;
         B = new FID[D];
@@ -51,6 +54,8 @@ public class WaveletMatrix {
      * The number of c-s in [0, pos)
      */
     public int rank(int c, int pos) {
+        if (c > M) return 0;
+
         int s = 0, e = pos;
         for (int i = D - 1; i >= 0; i--) {
             boolean cur = c << 31 - i < 0;
@@ -68,6 +73,8 @@ public class WaveletMatrix {
      * The index of the ind-th c (0-origin).
      */
     public int select(int c, int ind) {
+        if (c > M) return -1;
+
         int[] s = new int[D];
         for (int i = D - 1; i >= 1; i--) {
             boolean cur = c << 31 - i < 0;
@@ -84,5 +91,143 @@ public class WaveletMatrix {
         }
 
         return ind;
+    }
+
+    /**
+     * The maximum number in [s, e)
+     */
+    public int max(int s, int e) {
+        if (s >= e) return 0;
+        int res = 0;
+        for (int i = D - 1; i >= 0; i--) {
+            boolean one = B[i].rank(true, e) - B[i].rank(true, s) > 0;
+
+            s = B[i].rank(one, s);
+            e = B[i].rank(one, e);
+            if (one) {
+                res |= 1 << i;
+                s += k[i];
+                e += k[i];
+            }
+        }
+        return res;
+    }
+
+    /**
+     * The minimum number in [s, e)
+     */
+    public int min(int s, int e) {
+        if (s >= e) return Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = D - 1; i >= 0; i--) {
+            boolean one = B[i].rank(false, e) - B[i].rank(false, s) == 0;
+
+            s = B[i].rank(one, s);
+            e = B[i].rank(one, e);
+            if (one) {
+                res |= 1 << i;
+                s += k[i];
+                e += k[i];
+            }
+        }
+        return res;
+    }
+
+    /**
+     * The j-th largest number in [s, e)
+     */
+    public int quantile(int s, int e, int j) {
+        if (j >= e - s) throw new IllegalArgumentException("j >= e - s.  s: " + s + "  e: " + e + "  j: " + j);
+        int res = 0;
+        for (int i = D - 1; i >= 0; i--) {
+            int one = B[i].rank(true, e) - B[i].rank(true, s);
+            boolean cur = one > j;
+
+            s = B[i].rank(cur, s);
+            e = B[i].rank(cur, e);
+            if (cur) {
+                res |= 1 << i;
+                s += k[i];
+                e += k[i];
+            } else {
+                j -= one;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * #numbers ∈ [x, y) in [s, e).
+     */
+    public int rangefreq(int s, int e, int x, int y) {
+        x = Math.min(x, M + 1);
+        y = Math.min(y, M + 1);
+
+        return recurRF(D - 1, s, e, x, y, 0, (1 << D));
+    }
+
+    private int recurRF(int d, int s, int e, int x, int y, int lower, int upper) {
+        if (x == lower && y == upper) return e - s;
+        int mid = (lower + upper) >>> 1;
+        int res = 0;
+        if (x < mid) {
+            res += recurRF(d - 1, B[d].rank(false, s), B[d].rank(false, e), x, Math.min(y, mid), lower, mid);
+        }
+        if (y > mid) {
+            res += recurRF(d - 1, B[d].rank(true, s) + k[d], B[d].rank(true, e) + k[d], Math.max(x, mid), y, mid, upper);
+        }
+        return res;
+    }
+
+    /**
+     * Largest c in [s, e) such that x <= c < y. -1 on not found.
+     */
+    public int prevvalue(int s, int e, int x, int y) {
+        x = Math.min(x, M + 1);
+        y = Math.min(y, M + 1);
+
+        return recurPV(D - 1, s, e, x, y, 0, (1 << D));
+    }
+
+    private int recurPV(int d, int s, int e, int x, int y, int lower, int upper) {
+        if (e == s) return -1;
+        if (d < 0) {
+            return x;
+        }
+        int mid = (lower + upper) >>> 1;
+        if (y > mid) {
+            int res = recurPV(d - 1, B[d].rank(true, s) + k[d], B[d].rank(true, e) + k[d], Math.max(x, mid), y, mid, upper);
+            if (res >= 0) return res;
+        }
+        if (x < mid) {
+            return recurPV(d - 1, B[d].rank(false, s), B[d].rank(false, e), x, Math.min(y, mid), lower, mid);
+        }
+        return -1;
+    }
+
+    /**
+     * Smallest c in [s, e) such that x <= c < y. -1 on not found.
+     */
+    public int nextvalue(int s, int e, int x, int y) {
+        x = Math.min(x, M + 1);
+        y = Math.min(y, M + 1);
+
+        return recurNV(D - 1, s, e, x, y, 0, (1 << D));
+    }
+
+    private int recurNV(int d, int s, int e, int x, int y, int lower, int upper) {
+        if (e == s) return -1;
+        if (d < 0) {
+            return x;
+        }
+        int mid = (lower + upper) >>> 1;
+        if (x < mid) {
+            int res = recurNV(d - 1, B[d].rank(false, s), B[d].rank(false, e), x, Math.min(y, mid), lower, mid);
+            if (res >= 0) return res;
+        }
+        if (y > mid) {
+            return recurNV(d - 1, B[d].rank(true, s) + k[d], B[d].rank(true, e) + k[d], Math.max(x, mid), y, mid, upper);
+        }
+        return -1;
     }
 }
--- test/net/ogiekako/algorithm/dataStructure/WaveletMatrixTest.java
+++ test/net/ogiekako/algorithm/dataStructure/WaveletMatrixTest.java
@@ -23,68 +23,170 @@ public class WaveletMatrixTest {
         Assert.assertEquals(0, wm.rank(Integer.MAX_VALUE, 9));
 
         wm = new WaveletMatrix(new int[0]);
-        Assert.assertEquals(0, wm.rank(0,0));
+        Assert.assertEquals(0, wm.rank(0, 0));
         wm = new WaveletMatrix(new int[]{0});
-        Assert.assertEquals(1, wm.rank(0,1));
+        Assert.assertEquals(1, wm.rank(0, 1));
     }
 
     @Test
     public void select() {
-        WaveletMatrix wm = new WaveletMatrix(new int[]{0,7,2,1,4,3,6,7,2,5,0,4,7,2});
-        Assert.assertEquals(2, wm.select(2, 0));
-        Assert.assertEquals(8, wm.select(2, 1));
-        Assert.assertEquals(13, wm.select(2, 2));
-        Assert.assertEquals(-1, wm.select(2, 3));
-
+        WaveletMatrix wm = new WaveletMatrix(new int[]{0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2});
         Assert.assertEquals(0, wm.select(0, 0));
         Assert.assertEquals(1, wm.select(7, 0));
         Assert.assertEquals(2, wm.select(2, 0));
         Assert.assertEquals(3, wm.select(1, 0));
-        Assert.assertEquals(4, wm.select(4, 0));
         Assert.assertEquals(5, wm.select(3, 0));
         Assert.assertEquals(6, wm.select(6, 0));
-        Assert.assertEquals(7, wm.select(7, 1));
         Assert.assertEquals(8, wm.select(2, 1));
         Assert.assertEquals(9, wm.select(5, 0));
         Assert.assertEquals(10, wm.select(0, 1));
         Assert.assertEquals(11, wm.select(4, 1));
         Assert.assertEquals(12, wm.select(7, 2));
         Assert.assertEquals(13, wm.select(2, 2));
+        Assert.assertEquals(-1, wm.select(2, 3));
 
         wm = new WaveletMatrix(new int[0]);
-        Assert.assertEquals(-1, wm.select(0,0));
+        Assert.assertEquals(-1, wm.select(0, 0));
         wm = new WaveletMatrix(new int[]{0});
-        Assert.assertEquals(0, wm.select(0,0));
+        Assert.assertEquals(0, wm.select(0, 0));
+    }
+
+    @Test
+    public void max() {
+        WaveletMatrix wm = new WaveletMatrix(new int[]{0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2});
+        Assert.assertEquals(0, wm.max(0, 0));
+        Assert.assertEquals(6, wm.max(2, 7));
+        Assert.assertEquals(5, wm.max(8, 12));
+    }
+
+    @Test
+    public void min() {
+        WaveletMatrix wm = new WaveletMatrix(new int[]{0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2});
+        Assert.assertEquals(Integer.MAX_VALUE, wm.min(0, 0));
+        Assert.assertEquals(1, wm.min(2, 7));
+        Assert.assertEquals(0, wm.min(8, 12));
+        Assert.assertEquals(3, wm.min(4, 8));
+    }
+
+    @Test
+    public void quantile() {
+        WaveletMatrix wm = new WaveletMatrix(new int[]{0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2});
+        Assert.assertEquals(2, wm.quantile(0, 3, 1));
+        Assert.assertEquals(0, wm.quantile(0, 3, 2));
+        Assert.assertEquals(1, wm.quantile(0, 4, 2));
+        Assert.assertEquals(4, wm.quantile(2, 13, 5));
+    }
+
+    @Test
+    public void rangefreq() {
+        int[] S = {0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2};
+        WaveletMatrix wm = new WaveletMatrix(S.clone());
+        Assert.assertEquals(6, wm.rangefreq(2, 11, 2, 7));
+        Assert.assertEquals(14, wm.rangefreq(0, 14, 0, 8));
+
+        Random rnd = new Random(1209812L);
+        int n = S.length;
+        for (int i = 0; i < 100; ) {
+            int s = rnd.nextInt(n);
+            int t = rnd.nextInt(n);
+            if (s > t) continue;
+            int x = rnd.nextInt(8);
+            int y = rnd.nextInt(8);
+
+            int res = wm.rangefreq(s, t, x, y);
+            int exp = 0;
+            for (int j = s; j < t; j++)
+                if (x <= S[j] && S[j] < y) {
+                    exp++;
+                }
+            Assert.assertEquals(s + " " + t + " " + x + " " + y, exp, res);
+
+            i++;
+        }
+    }
+
+    @Test
+    public void prevvalue() {
+        int[] S = {0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2};
+        WaveletMatrix wm = new WaveletMatrix(S.clone());
+        Assert.assertEquals(6, wm.prevvalue(2, 11, 2, 7));
+        Assert.assertEquals(7, wm.prevvalue(0, 14, 0, 8));
+        Assert.assertEquals(4, wm.prevvalue(2, 9, 2, 6));
+    }
+
+    @Test
+    public void nextvalue() {
+        int[] S = {0, 7, 2, 1, 4, 3, 6, 7, 2, 5, 0, 4, 7, 2};
+        WaveletMatrix wm = new WaveletMatrix(S.clone());
+        Assert.assertEquals(2, wm.nextvalue(2, 11, 2, 7));
+        Assert.assertEquals(0, wm.nextvalue(0, 14, 0, 8));
+        Assert.assertEquals(2, wm.nextvalue(4, 10, 1, 6));
     }
 
     @Test
     public void stress() throws Exception {
-        int n = 1000;
-        int m = 65;
-        int[] s = new int[n];
         Random rnd = new Random(102498109248L);
-        for (int i = 0; i < n; i++) {
-            s[i] = rnd.nextInt(m);
-        }
-        int[][] rank = new int[m][n + 1];
-        int[][] select = new int[m][n + 1];
-        ArrayUtils.fill(select, -1);
-        for (int i = 0; i < m; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                rank[i][j + 1] = rank[i][j];
-                if (s[j] == i) {
-                    rank[i][j + 1]++;
-                    select[i][cnt++] = j;
+        for (int n : new int[]{1, 2, 3, 10, 1000}) {
+            int m = 65;
+            int[] S = new int[n];
+            for (int i = 0; i < n; i++) {
+                S[i] = rnd.nextInt(m);
+            }
+            int[][] rank = new int[m][n + 1];
+            int[][] select = new int[m][n + 1];
+            ArrayUtils.fill(select, -1);
+            for (int i = 0; i < m; i++) {
+                int cnt = 0;
+                for (int j = 0; j < n; j++) {
+                    rank[i][j + 1] = rank[i][j];
+                    if (S[j] == i) {
+                        rank[i][j + 1]++;
+                        select[i][cnt++] = j;
+                    }
                 }
             }
-        }
 
-        WaveletMatrix wm = new WaveletMatrix(s);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                Assert.assertEquals(i + " " + j, rank[i][j], wm.rank(i, j));
-                Assert.assertEquals(i + " " + j, select[i][j], wm.select(i, j));
+            WaveletMatrix wm = new WaveletMatrix(S.clone());
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n + 1; j++) {
+                    Assert.assertEquals(i + " " + j, rank[i][j], wm.rank(i, j));
+                    Assert.assertEquals(i + " " + j, select[i][j], wm.select(i, j));
+                }
+            }
+
+            for (int i = 0; i < 100; ) {
+                int s = rnd.nextInt(n);
+                int t = rnd.nextInt(n);
+                if (s > t) continue;
+                int x = rnd.nextInt(m + 1);
+                int y = rnd.nextInt(m + 1);
+
+                int res = wm.rangefreq(s, t, x, y);
+                int exp = 0;
+                for (int j = s; j < t; j++)
+                    if (x <= S[j] && S[j] < y) {
+                        exp++;
+                    }
+                Assert.assertEquals(s + " " + t + " " + x + " " + y, exp, res);
+
+                res = wm.prevvalue(s, t, x, y);
+                exp = -1;
+                for (int j = s; j < t; j++)
+                    if (x <= S[j] && S[j] < y) {
+                        exp = Math.max(exp, S[j]);
+                    }
+                Assert.assertEquals(s + " " + t + " " + x + " " + y, exp, res);
+
+                res = wm.nextvalue(s, t, x, y);
+                exp = Integer.MAX_VALUE;
+                for (int j = s; j < t; j++)
+                    if (x <= S[j] && S[j] < y) {
+                        exp = Math.min(exp, S[j]);
+                    }
+                exp = exp == Integer.MAX_VALUE ? -1 : exp;
+                Assert.assertEquals(s + " " + t + " " + x + " " + y, exp, res);
+
+                i++;
             }
         }
     }
